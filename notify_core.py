0a7e90db2ab72f04d80cbe16e77187fc771991d7
Nakultt <nakulnuked@gmail.com>
2025-04-27 21:32:33 +0530

    Refactor notification scripts for improved clarity and maintainability

--- notify_core.py
+++ notify_core.py
@@ -3,10 +3,10 @@ from email_send import send_expiry_alert_email
 
 def notify_expiry(XML_FILE, DAYS_THRESHOLD):
     print("\nChecking expiration dates...")
-    near_expiry_list = check_expiry()
+    near_expiry_list = check_expiry(XML_FILE, DAYS_THRESHOLD)
 
     print("\nSending email if needed...")
     if not send_expiry_alert_email(near_expiry_list):
         print("Failed to send email (if needed).")
 
-        print("\nProcess completed.")
+    print("\nProcess completed.")
